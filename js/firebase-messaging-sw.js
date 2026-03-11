3ae16489ec2cd1d8c813beb09a359f0001d80d26
Imaginary Gifts <imaginarygifts.in@gmail.com>
2026-03-11 19:46:04 +0530

    Update firebase-messaging-sw.js

--- js/firebase-messaging-sw.js
+++ js/firebase-messaging-sw.js
@@ -2,11 +2,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyATNHNTN0S_otYHNGuydqOjcr1AhCgD6yc",
+  authDomain: "imaginary-gifts.firebaseapp.com",
+  projectId: "imaginary-gifts",
+  storageBucket: "imaginary-gifts.firebasestorage.app",
+  messagingSenderId: "759826392629",
+  appId: "1:759826392629:web:9d9bbe53c8ab36ad07737c"
 });
 
 const messaging = firebase.messaging();
